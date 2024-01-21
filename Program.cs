88bcbe60fa3c59032fc8cdf8122b259b068c3a42
Anand Kunal Mishra <73222935+anandkunalmishra@users.noreply.github.com>
2024-01-21 16:52:16 +0530

    Employee Wage Computation Program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Employee
+{
+    class Payrole
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Hello Employee!");
+
+            Console.ReadLine();
+        }
+    }
+}
